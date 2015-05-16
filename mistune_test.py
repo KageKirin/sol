2ddc51a0af1fe1cc55a0966f3d1886d08b81d16c
Chris Helmich <kagekirin@gmail.com>
2015-05-17 01:40:37 +0900

    added mistune_test.py

---
+++ mistune_test.py
@@ -0,0 +1,15 @@
+#!/bin/python
+import os
+import os.path
+import markdown
+
+if __name__ == '__main__':
+	root = os.path.dirname(__file__)
+	filepath = os.path.join(
+		root, 'markdown_documentation_syntax.text'
+	)
+	with open(filepath, 'r') as f:
+		text = f.read()
+	
+	d = mistune.markdown(text)
+	print d

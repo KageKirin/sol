2be8a8f64afeb1791bc10c7b0d3c85f5f40a95de
Chris Helmich <kagekirin@gmail.com>
2015-05-19 00:28:12 +0900

    added support for multi-document markdown files

--- md2json.py
+++ md2json.py
@@ -7,14 +7,31 @@ import yaml
 import json
 
 def md2dict(text):
+	"""
+	splits then text into YAML front matter config and markdown content
+	"""
 	config, content = text.split('\n---')
 	data = dict()
-	data['config'] = yaml.load(config)	
+	data['config'] = yaml.load(config)
 	data['content'] = content
+	return data
+
+def multimd2dict(text):
+	"""
+	splits then text into YAML front matter config and several markdown content parts
+	"""
+	parts = text.split('\n---')
+	data = dict()
+	data['config'] = yaml.load(parts[0])
+	data['content'] = parts[1:]
 	return data
 
 def md2json(text):
 	data = md2dict(text)
+	return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+
+def multimd2json(text):
+	data = multimd2dict(text)
 	return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
 
